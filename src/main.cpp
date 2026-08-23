8485d72ebfca5e202acaff0fa6346eddd920e21e
elbichorecuperar-lang <elbichorecuperar@gmail.com>
2026-08-23 19:48:49 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
-    void setupHashtag() {
-        PlayLayer::setupHashtag();
-        // Aquí irá la lógica de los checkpoints automáticos
+    // Esta función se ejecuta cuando inicias un nivel
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
+            return false;
+        }
+        
+        // Mensaje de prueba para saber que el mod está activo en el juego
+        Notification::create("Auto Checkpoint activado!", NotificationIcon::Success)->show();
+        
+        return true;
     }
 };
