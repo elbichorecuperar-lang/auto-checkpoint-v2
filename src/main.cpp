ec173c7073343cfa59d4b2832bb00c0a24272cae
elbichorecuperar-lang <elbichorecuperar@gmail.com>
2026-08-23 20:25:51 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/PlayLayer.hpp>
+include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyPlayLayer, PlayLayer) {
-    // Esta función se ejecuta cuando inicias un nivel
-    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
-            return false;
-        }
-        
-        // Mensaje de prueba para saber que el mod está activo en el juego
-        Notification::create("Auto Checkpoint activado!", NotificationIcon::Success)->show();
-        
+$modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+        log::info("Auto Checkpoint cargado correctamente!");
         return true;
     }
 };
